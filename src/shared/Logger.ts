71a72eaa558ca420f3894fb8c6f0afe21a1e7194
Reinhard Bronner <reinhard_bronner@t-online.de>
2020-04-15 11:49:16 +0200

    Adjust logger

--- src/shared/Logger.ts
+++ src/shared/Logger.ts
@@ -19,7 +19,7 @@ const logger = createLogger({
  * to `combined.log. Write all logs error (and below) to `error.log`.
  * For development, print to the console.
  */
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
 
     const fileFormat = format.combine(
         format.timestamp(),
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
     logger.add(errTransport);
     logger.add(infoTransport);
 
-} else {
+} else {*/
 
     const errorStackFormat = format((info) => {
         if (info.stack) {
@@ -55,6 +55,6 @@ if (process.env.NODE_ENV === 'production') {
         ),
     });
     logger.add(consoleTransport);
-}
+//}
 
 export default logger;
