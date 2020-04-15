2e4c90ccebdb68fd70fb9a4e3a301894c4048a28
Reinhard Bronner <reinhard_bronner@t-online.de>
2020-04-15 21:15:43 +0200

    tried fixing

--- build.js
+++ build.js
@@ -7,8 +7,15 @@ try {
     fs.removeSync('./dist/');
     // Copy front-end files
     fs.copySync('./src/views', './dist/views');
-    // Transpile the typescript files
-    childProcess.exec('tsc --build tsconfig.prod.json');
 } catch (err) {
     console.log(err);
 }
+// Transpile the typescript files
+childProcess.exec('tsc --build tsconfig.prod.json', (err, stdout, stderr) => {
+    if (err) {
+        console.error(err)
+      } else {
+       console.log(`stdout: ${stdout}`);
+       console.log(`stderr: ${stderr}`);
+      }
+});
