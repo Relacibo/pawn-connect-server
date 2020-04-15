70ea16d1f8b76e8e207a169b862fc875c9232256
Reinhard Bronner <reinhard_bronner@t-online.de>
2020-04-15 11:22:33 +0200

    fix

--- util/build.js
+++ util/build.js
@@ -4,9 +4,9 @@ const childProcess = require('child_process');
 
 try {
     // Remove current build
-    fs.removeSync('./app/');
+    fs.removeSync('./dist/');
     // Copy front-end files
-    fs.copySync('./src/views', './app/views');
+    fs.copySync('./src/views', './dist/views');
     // Transpile the typescript files
     childProcess.exec('tsc --build tsconfig.prod.json');
 } catch (err) {
